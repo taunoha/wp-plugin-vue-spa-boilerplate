f44173a75dcb7d50a1f978fffc4f43d63aa27e57
Tauno Hanni <taunoh@gmail.com>
2024-11-07 14:07:32 +0200

    feat: ESlint flat

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,9 @@ export default defineConfig({
         './src/composables/**',
       ],
       vueTemplate: true,
+      eslintrc: {
+        enabled: true,
+      },
     }),
     Components({
       deep: true,
