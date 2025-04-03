58414450212d5c8576f2295304c915db43baee1d
Tauno Hanni <taunoh@gmail.com>
2025-04-03 17:12:00 +0300

    chore: Vite conf update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import { fileURLToPath, URL } from 'url';
+import replace from '@rollup/plugin-replace';
 
 import { unheadVueComposablesImports } from '@unhead/vue';
 import vue from '@vitejs/plugin-vue';
@@ -12,66 +13,74 @@ import prepareHMR from './vite-plugins/prepare-hmr.js';
 const filename = '{plugin-shortcode}';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: [
-        'vue',
-        '@vueuse/core',
-        'vue-router',
-        unheadVueComposablesImports
-      ],
-      dirs: [
-        './src/utils/**',
-        './src/composables/**',
-      ],
-      vueTemplate: true,
-      eslintrc: {
-        enabled: true,
-      },
-    }),
-    Components({
-      deep: true,
-      directoryAsNamespace: true,
-    }),
-    gettextExtractorForWordpress({
-      path: path.resolve(__dirname, 'languages/'),
-      context: '',
-      domain: '{plugin-shortcode}'
-    }),
-    prepareHMR(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    cors: false,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    }
-  },
-  build: {
-    lib: {
-      name: '{plugin}',
-      entry: path.resolve(__dirname, `src/main.js`),
-      formats: ['es'],
-      fileName: filename
+export default defineConfig(() => {
+  const env = { ...process.env };
+  return {
+    define: {
+      'process.env': env
     },
-    rollupOptions: {
-      output: {
-        extend: true,
-        assetFileNames: (assetInfo) => {
-          if( assetInfo.name == 'style.css') {
-            return `${filename}.css`;
-          }
-          return assetInfo.name;
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: [
+          'vue',
+          '@vueuse/core',
+          'vue-router',
+          unheadVueComposablesImports
+        ],
+        dirs: [
+          './src/utils/**',
+          './src/composables/**',
+        ],
+        vueTemplate: true,
+        eslintrc: {
+          enabled: true,
         },
+      }),
+      Components({
+        deep: true,
+        directoryAsNamespace: true,
+      }),
+      gettextExtractorForWordpress({
+        path: path.resolve(__dirname, 'languages/'),
+        context: '',
+        domain: '{plugin-shortcode}'
+      }),
+      prepareHMR(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      cors: false,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
+    },
+    build: {
+      lib: {
+        name: '{plugin}',
+        entry: path.resolve(__dirname, `src/main.js`),
+        formats: ['es'],
+        fileName: filename
+      },
+      rollupOptions: {
+        plugins: [
+          replace({
+            preventAssignment: true,
+            delimiters: ['', ''],
+            include: 'src/**/*.vue',
+            values: {
+              [process.cwd()]: '',
+              [__dirname]: '',
+            },
+          })
+        ],
+        output: {
+          extend: true,
+        }
       }
     }
   }
